b23660744d80ee501d75f71cd7256c19d662e14d
Kartik Sharma <91308606+Kartiksharma88@users.noreply.github.com>
2023-12-10 19:20:10 +0530

    Update Exp.-4(a) Program to calculate factorial of a number using a for loop.c

--- Exp.-4(a) Program to calculate factorial of a number using a for loop.c
+++ Exp.-4(a) Program to calculate factorial of a number using a for loop.c
@@ -4,7 +4,7 @@ int main()
     int c=1;  //Code to find Factorial 
     int a=5;
     
-    for (int i=1; i<a; i++)
+    for (int i=1; i<=a; i++)
     {   
         c=c*i;
         printf("%d\n", c);
